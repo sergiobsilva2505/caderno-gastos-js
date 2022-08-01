f567202a95100283597c4c34d258c8d74a5ee57f
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2022-08-01 00:07:06 -0300

    feat: caderninho

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let resumoDoMes = document.querySelector("#resumo-do-mes");
+let receitas = document.querySelector("#receitas");
+let despesasFixas = document.querySelector("#despesas-fixas");
+let despesasEventuais = document.querySelector("#despesas-eventuais");
+let cartaoNubank = document.querySelector("#credito-nubank");
+let santanderDora = document.querySelector("#santander-dora");
+let santanderSx = document.querySelector("#credito-sx");
+let santanderGeraldo = document.querySelector("#santander-geraldo");
+
+let subTitulo = document.querySelector(".subTitulo");
+
+let tituloTabrla = document.querySelector(".titulo-tabela");
